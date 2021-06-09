c21eaaa0bb83911e567db335a67508ba037d7395
Shiva Kumar H R <shivahr@gmail.com>
2021-06-09 14:18:36 +0530

    Moved the scripts to OCR-Webp-App project

--- scripts/run_couchdb_docker.sh
+++
@@ -1,3 +0,0 @@
-export COUCHDB_PORT="${COUCHDB_PORT:-5984}"
-docker stop couchdb
-docker run -d --rm -p ${COUCHDB_PORT}:5984 -v /home/couchdb/data:/opt/couchdb/data -v /home/couchdb/etc:/opt/couchdb/etc/local.d --name couchdb couchdb:3
