99d57912dabe8d48d6f18ca9cb2978778bd6d46b
Shiva Kumar H R <shivahr@gmail.com>
2021-06-12 09:46:15 +0530

    Fix conflict

--- scripts/run_ocr_engine_docker.sh
+++ scripts/run_ocr_engine_docker.sh
@@ -1,2 +1,2 @@
 docker stop ocr-engine
-docker run -d --restart unless-stopped --rm --name ocr-engine --network=container:ocr-api mile-ocr-engine
+docker run -d --restart unless-stopped --name ocr-engine --network=container:ocr-api mile-ocr-engine
